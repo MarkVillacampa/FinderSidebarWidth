975bb154f854021eb6b61313cf45228c22584366
Mark Villacampa <markvjal@gmail.com>
2015-07-11 03:50:23 +0200

    remove stuff

--- FinderSidebarWidth/FinderSidebarWidth.m
+++ FinderSidebarWidth/FinderSidebarWidth.m
@@ -1,11 +1,3 @@
-//
-//  FinderSidebarWidth.m
-//  FinderSidebarWidth
-//
-//  Created by Mark Villacampa on 16/06/15.
-//  Copyright 2015. All rights reserved.
-//
-
 #import "FinderSidebarWidth.h"
 
 @implementation FinderSidebarWidth
